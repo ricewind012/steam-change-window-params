0436776f78a73989927ee7996aca2df8946c211d
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2026-09-10 11:41:38 +0000

    fix: update webpack modules

--- src/modules/localization.ts
+++ src/modules/localization.ts
@@ -4,4 +4,6 @@ import { findModuleExport } from "@steambrew/client";
  * Replaces `%1$s`, `%2$s`, etc. in a localization string with provided arguments.
  */
 export const Localize: (strToken: string, ...args: string[]) => string =
-	findModuleExport((m) => m.toString().includes("LocalizeString(e);return"));
+	findModuleExport((m) =>
+		m.toString().match(/LocalizeString\([\w$]+\);return/),
+	);
