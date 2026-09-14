32a187f17818e11989f862404c3de5d2db9c59b1
ricewind012 <58827198+ricewind012@users.noreply.github.com>
2026-09-14 16:37:13 +0100

    wtf

--- src/frontend/consts.ts
+++
@@ -1,9 +0,0 @@
-export const PLUGIN_NAME = "steam-change-window-params";
-export const PLUGIN_PATH = (() => {
-	const script = document.querySelector(
-		`script[src*="${PLUGIN_NAME}"]`,
-	) as HTMLScriptElement;
-	const { href } = new URL(script.src);
-
-	return href.replace(/\.millennium\/Dist\/index.js$/, "");
-})();
